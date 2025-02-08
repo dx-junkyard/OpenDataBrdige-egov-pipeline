15d20c9a4b242ce99c2280e724206c869dfec367
urashin <s.urakawa@hotmail.co.jp>
2025-02-08 18:24:48 +0900

    feat: add embedding step

--- pipeline/embedding_step.py
+++ pipeline/embedding_step.py
@@ -15,18 +15,18 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 
 class EmbeddingStep:
     def __init__(self, step_config):
-        self.service_catalog_file = step_config['service_catalog_json']
+        self.law_catalog_file = step_config['law_catalog_json']
         self.embeddings_file = step_config['embeddings_file']
         self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
         self.model = BertModel.from_pretrained('bert-base-uncased')
 
-    # service catalog jsonの読み込み
+    # law catalog jsonの読み込み
     def load_json_data(self, file_path):
         """JSONファイルを読み込む"""
         with open(file_path, 'r', encoding='utf-8') as f:
             return json.load(f)
 
-    # service catalog
+    # law catalog
     def save_embeddings_to_file(self, embeddings, entries, output_file):
         """ベクトルデータをJSONファイルに保存する"""
         data = {
@@ -50,56 +50,61 @@ class EmbeddingStep:
     # 渡されたtextをベクトル化
     def get_embedding(self, text):
         """テキストをベクトル化する"""
+        if isinstance(text, list):  # リストなら結合して1つの文字列にする
+            text = " ".join(text)
         inputs = self.tokenizer(text, return_tensors='pt', max_length=512, truncation=True)
         outputs = self.model(**inputs)
         embedding = outputs.last_hidden_state.mean(dim=1)
         return embedding.detach().numpy()
 
-    # service catalog中の全概要をvector化
-    def get_overview_embeddings(self, service_catalog):
+    def extract_text(self, law):
+        """Lawオブジェクトから特定のフィールドのテキストを抽出"""
+        texts = []
+        fields = ['@Abbrev', '#text', 'EnactStatement', 'ChapterTitle', 'ArticleCaption', 'ArticleTitle', 'Sentence']
+
+        for key, value in law.items():
+            if key in fields and isinstance(value, str):
+                texts.append(value)
+            elif isinstance(value, dict):
+                texts.extend(self.extract_text(value))
+            elif isinstance(value, list):
+                for item in value:
+                    if isinstance(item, dict):
+                        texts.extend(self.extract_text(item))
+        return texts
+
+    # law catalog中の全概要をvector化
+    def get_overview_embeddings(self, law_catalog):
         """全ての概要をベクトル化する"""
         overview_embeddings = []
         entries = []
 
-        for service in service_catalog:
-            #logging.info(f"Service data: {service}")
+        for law in law_catalog:
+            #logging.info(f"Service data: {law}")
 
-            overview_data = service.get("概要", {}).get("items", [])
-            if isinstance(overview_data, list):
-                overview = " ".join(overview_data)
-            elif isinstance(overview_data, str):
-                overview = overview_data
-            else:
-                continue  # 不正な形式はスキップ
+            law = law.get("Law")
+            overview = self.extract_text(law)
 
             if overview:
                 embedding = self.get_embedding(overview)
                 overview_embeddings.append(embedding)
 
-                formal_name_data = service.get("正式名称")
-                if isinstance(formal_name_data, dict) and "items" in formal_name_data:
-                    items = formal_name_data.get("items")
-                    if items is None:
-                        formal_name = "N/A"
-                    elif isinstance(items, list):
-                        formal_name = items[0] if items else "N/A"
-                    else:
-                        formal_name = str(items)
-                else:
-                    formal_name = "N/A"
+                law_num = law.get("LawNum")
+                law_title = law.get("LawBody").get("LawTitle").get("@Abbrev")
+                law_text = law.get("LawBody").get("LawTitle").get("#text")
     
                 entry = {
-                    'overview': overview,
-                    'formal_name': formal_name,
-                    'url': service.get("URL", {}).get("items", "N/A")
+                    'law_num': law_num,
+                    'law_title': law_title,
+                    'law_text': law_text
                 }
                 entries.append(entry)
 
         return np.vstack(overview_embeddings), entries
 
     def execute(self):
-        self.service_catalog = self.load_json_data(self.service_catalog_file)
-        self.overview_embeddings, self.entries = self.get_overview_embeddings(self.service_catalog)
+        self.law_catalog = self.load_json_data(self.law_catalog_file)
+        self.overview_embeddings, self.entries = self.get_overview_embeddings(self.law_catalog)
         if self.embeddings_file:
             self.save_embeddings_to_file(self.overview_embeddings, self.entries, self.embeddings_file)
 
